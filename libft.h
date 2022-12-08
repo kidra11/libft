77b1e2b1f7770bc3be25f9d84d83cdb3289e7296
shiro kubo <nathalie1116kubo@gmail.com>
2022-12-08 14:04:53 +0100

    libft.h

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: marvin <shiro@student.42.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 17:29:26 by marvin            #+#    #+#             */
-/*   Updated: 2022/12/07 19:17:28 by marvin           ###   ########.fr       */
+/*   Updated: 2022/12/08 13:45:42 by marvin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,20 @@
 # include <unistd.h>
 # include <string.h>
 
-int	ft_isalnum(int c);
-int	ft_isalpha(int c);
-int	ft_isascii(int c);
-int	ft_isdigit(int c);
-int	ft_isprint(int c);
-size_tft_strlcat(char *dst, const char *src, size_t dstsize);
-size_tft_strlcpy(char *dst, const char *src, size_t dstsize);
-size_tft_strlen(const char *s);
+int		ft_isalnum(int c);
+
+int		ft_isalpha(int c);
+
+int		ft_isascii(int c);
+
+int		ft_isdigit(int c);
+
+int		ft_isprint(int c);
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
+size_t	ft_strlen(const char *s);
 
 #endif
