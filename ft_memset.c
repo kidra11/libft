a84d2e84d813b85dadad51f69a4d761d5faf8acb
shiro kubo <nathalie1116kubo@gmail.com>
2023-02-03 15:02:18 +0100

    int *

--- ft_memset.c
+++ ft_memset.c
@@ -6,13 +6,13 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 15:17:33 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/01 15:20:02 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/03 14:57:30 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *b, int *c, size_t len)
+void	*ft_memset(void *b, int c, size_t len)
 {
 	char	*unb;
 
@@ -25,3 +25,13 @@ void	*ft_memset(void *b, int *c, size_t len)
 	}
 	return (b);
 }
+
+/*
+#include <stdio.h>
+int main()
+{
+	char str[] = "salut espece de 3amar";
+
+	printf("%s", (char *)ft_memset(str, '4', 5));
+	return 0;
+}*/
