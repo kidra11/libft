d1ff1f31aabfe14851e8101689a091c4f41c2c19
shiro kubo <nathalie1116kubo@gmail.com>
2023-02-04 19:15:57 +0100

    ft_strnstr.c_OK

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/02 17:13:57 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/04 11:10:50 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/04 19:12:25 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,18 @@ char	*ft_strnstr(const char *big, const char *little, size_t len)
 	size_t	y;
 
 	i = 0;
-	y = 0;
-	while (len > 0)
+	if (*little == 0)
+		return ((char *)big);
+	while (big[i])
 	{
-		if (big[i] == little[y])
+		y = 0;
+		while (big[i + y] == little[y] && len > (i + y))
+		{
 			y++;
+		}
 		if (!little[y])
-			return ((char *)&big[i]);
-		len--;
+			return ((char *)big + i);
 		i++;
 	}
-	return (0);
+	return (NULL);
 }
