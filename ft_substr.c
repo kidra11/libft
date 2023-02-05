9504001d69b3565b1cce18e4b48b85e06cc6df4b
Nathalie Sion <nsion@c3r4p1.42perpignan.fr>
2023-02-05 22:48:40 +0100

    substr...

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 15:07:28 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/05 16:07:46 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/05 22:48:03 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,14 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*nv;
 	int		i;
 
-	nv = (char *) malloc((len + 1) * sizeof(char *));
+	if (start > ft_strlen(s))
+		return (NULL);
+	nv = (char *) malloc((len + 1) * sizeof(char));
 	if (nv == 0)
 		return (NULL);
 	i = 0;
-	if (start > ft_strlen(s))
-		return (NULL);
 	else if (len > ft_strlen(s))
-		return (NULL);
+		nv = (char *)s + start;
 	while (len-- > 0 && s[start])
 		nv[i++] = s[start++];
 	return (nv);
