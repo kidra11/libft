4d4c7d731f46e354ae499bee2f89e0d8ac7f5863
Nathalie Sion <nsion@c3r5p4.42perpignan.fr>
2023-02-08 20:07:26 +0100

    itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,18 +6,35 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 15:53:11 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/08 16:40:52 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/08 20:06:54 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	size_int(int n)
+{
+	int	k;
+
+	k = 1;
+	if (n < 0)
+		k++;
+	while (n > 0)
+	{
+		n / 10;
+		k++;
+	}
+	return (k);
+}
+
 char	*ft_itoa(int n)
 {
 	char	*str;
 
-	str = malloc();
+	
+	str = malloc((size_int(n) + 1) * sizeof(char);
 	if (str == 0)
 		return (NULL);
+
 	return (str);
 }
