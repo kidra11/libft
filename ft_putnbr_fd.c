509386974e4a8de5bb7f31cd5d06d61431f3dcbd
Nathalie Sion <nsion@c3r5p4.42perpignan.fr>
2023-02-12 15:54:16 +0100

    putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/12 15:14:14 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/12 15:49:56 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/12 15:54:08 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		ft_putchat_fd("-2147483648", fd);
+		ft_putchar_fd("-2147483648", fd);
 		return ;
 	}
 	if (n < 0)
@@ -25,7 +25,7 @@ void	ft_putnbr_fd(int n, int fd)
 		n = -n;
 	}
 	if (n >= 0 && n <= 9)
-		ft_putchr_fd((n + '0'), fd);
+		ft_putchar_fd((n + '0'), fd);
 	else if (n > 9)
 	{
 		ft_putnbr_fd((n / 10), fd);
