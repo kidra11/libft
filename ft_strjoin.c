f07410382bd0dc68e44d200e63175d7ca3ac9d44
Nathalie Sion <nsion@c3r5p4.42perpignan.fr>
2023-02-14 14:13:23 +0100

    strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 17:53:51 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/14 13:52:18 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/14 14:12:32 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,13 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	if (str == 0)
 		return (NULL);
 	l = 0;
-	while (s2[i])
+	k = 0;
+	while (s2[k])
 	{
-		if (i == l)
+		if (i == l && s1[i])
 			str[l++] = s1[i++];
-		if (i < l)
-			str[l++] = s2[i++];
+		else
+			str[l++] = s2[k++];
 	}
 	str[l] = '\0';
 	return (str);
