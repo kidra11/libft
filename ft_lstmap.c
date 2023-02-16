ff7b5758ccbba6f5a21f3e3790624d04d99e59cb
Nathalie Sion <nsion@c3r4p3.42perpignan.fr>
2023-02-16 11:07:44 +0100

    all

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 10:18:52 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/16 11:04:53 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/16 11:07:34 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*temp;
 
 	if (!lst)
-		return ;
+		return (0);
 	nv = lst;
 	while (lst->next != 0)
 	{
