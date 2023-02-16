91a93c059d371b373bac6dad38e50aee9f0c1b52
Nathalie Sion <nsion@c3r4p3.42perpignan.fr>
2023-02-16 12:56:44 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: nsion <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/05 15:59:14 by nsion             #+#    #+#             */
-/*   Updated: 2023/02/07 11:31:39 by nsion            ###   ########.fr       */
+/*   Updated: 2023/02/16 12:55:03 by nsion            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	if (!nmemb || !size)
 		return (malloc(0));
 	if (nmemb * size / nmemb != size)
-		return (malloc(0));
+		return (0);
 	m = malloc(nmemb * size);
 	if (m)
 		ft_bzero(m, nmemb * size);
